53e027ea0daea20bf353ffb74f8c2da23e6ddf40
Mithrandir712 <jonahsucksatemail@gmail.com>
2015-06-10 11:08:26 -0500

    Update game.js

--- _js/game.js
+++ _js/game.js
@@ -1,5 +1,11 @@
 var BubbleShoot = window.BubbleShoot || {};
 BubbleShoot.Game = (function($){
-  var Game = function(){}
+  var Game = function(){
+    this.init = function(){
+      $(".but_start_game").bind("click",startGame);
+    };
+    var startGame = function(){
+    };
+  };
   return Game;
 })(jquery);
