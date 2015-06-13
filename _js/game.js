01f7245f6dc89d305711b4a33174136ff418bd1d
Jonah <jonahsucksatemail@gmail.com>
2015-06-13 16:11:21 -0500

    Update game.js

--- _js/game.js
+++ _js/game.js
@@ -1,12 +1,20 @@
 var BubbleShoot = window.BubbleShoot || {};
 BubbleShoot.Game = (function($){
   var Game = function(){
+    var curBubble;
     this.init = function(){
       $(".but_start_game").bind("click",startGame);
     };
     var startGame = function(){
       $(".but_start_game").unbind("click");
       BubbleShoot.ui.hideDialog();
+      curBubble = getNextBubble();
+    };
+    var getNextBubble = function(){
+      var bubble = BubbleShoot.Bubble.create();
+      bubble.getSprite().addClass("cur_bubble");
+     $("#board").append(bubble.getSprite());
+      return bubble;
     };
   };
   return Game;
