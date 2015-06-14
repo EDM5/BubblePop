76b847bbf9f961612bbffa05daeed6e38dbf02c2
Jonah <jonahsucksatemail@gmail.com>
2015-06-13 20:36:15 -0500

    Update ui.js

--- _js/ui.js
+++ _js/ui.js
@@ -1,11 +1,15 @@
 var BubbleShoot = window.BubbleShoot || {}
 BubbleShoot.ui = (function($){
   var ui = {
+    BUBBLE_DIMS : 44,
     init : function(){
     },
     hideDialog : function(){
      $(".dialog").fadeOut(300);
-    }
+    },
+    getMouseCoords : function(e){
+      var coords = {x : e.pageX, y : e.pageY};
+    },
   };
   return ui;
 })(jQuery);
